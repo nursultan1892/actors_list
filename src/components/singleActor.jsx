89f0154b95865a05e5153643cbaa48fda5217725
Nursultan <nursultan18.92@gmail.com>
2023-10-24 13:32:59 +0400

    popularity

--- src/components/singleActor.jsx
+++ src/components/singleActor.jsx
@@ -10,7 +10,7 @@ export default class SingleActor extends Component {
       <div className="container">
         <img src={`https://image.tmdb.org/t/p/w185${profile_path}`} alt="" />
         <h4>{name}</h4>
-        <p>Popularity Rate: {Math.round(popularity)}</p>
+        <p>Popularity: {Math.round(popularity)}</p>
         <div className="ModalComp">
           <ModalComp known_for={known_for} />
         </div>
